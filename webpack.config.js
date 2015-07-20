d1e6476fa12f7294a595434bdb2b116408f50902
vinhbachsy <bsdvinhuit@gmail.com>
2015-07-20 18:25:31 +0800

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,48 @@
-var path = require('path');
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/main.js',
+var path = require('path'),
+    merge = require('webpack-merge'),
+    TARGET = process.env.TARGET,
+    ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+    entry: path.resolve(ROOT_PATH, 'src/main.js'),
     output: {
-        path: path.join(__dirname, 'build'),
+        path: path.resolve(ROOT_PATH, 'build'),
         filename: 'bundle.js'
+    },
+    // devtool: "source-map",
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.resolve(ROOT_PATH, 'src'),
+                loader: "babel-loader"
+            },
+            {
+                test: /\.jsx$/,
+                include: path.resolve(ROOT_PATH, 'src'),
+                loaders: ["react-hot-loader", "babel-loader"]
+            },
+            {
+                test: /.scss$/,
+                // loader: ExtractTextPlugin.extract(
+                //     'css?sourceMap!sass?sourceMap'
+                // ),
+                include: path.resolve(ROOT_PATH, 'src'),
+                loader: 'style!css!sass'
+            }
+        ]
     }
+    // plugins: [
+    //     new ExtractTextPlugin("bundle.css")
+    // ]
+};
+
+if (TARGET === 'build') {
+    module.exports = common;
+}
+
+if (TARGET === 'dev') {
+    module.exports = common;
 }
